bfcd2f7dc1f257fc84e881428ac503061f520455
Therese C Kjær <69575527+ThereseCamK@users.noreply.github.com>
2024-10-06 23:03:07 +0200

    styiling og inclick på bilder

--- controller/viewController.js
+++ controller/viewController.js
@@ -19,7 +19,7 @@ let html = ''
         var first = i % 3 == 0 ? 'first' : ' ';
        html += `<div class="singleProduct ${first}">
                 <p> ${model.products[i].name}</p>
-                <img class="pictures" src="${model.products[i].img[0]}"/>
+                <img onclick="enlargeImg(this)" class="pictures" src="${model.products[i].img[0]}"/>
                
                 <p>Pris: ${model.products[i].price}</p>
                 <p>Beskrivelse: ${model.products[i].description}</p>
@@ -37,11 +37,11 @@ function Galleri(){
     let html = `
     `
   for(let i = 0; i < model.galleri.length; i++){
-       html += `<div class="singleProduct">
+       html += `<div class="singleProduct" >
                 <p> ${model.galleri[i].name}</p>
-               
-               <img class="pictures" src="${model.galleri[i].img[0]}"/>
-                <p>Beskrivelse: ${model.galleri[i].description}</p>
+          
+               <img class="pictures" onclick="enlargeImg(this)" src="${model.galleri[i].img[0]}"/><br>
+                <button class="moreBtn btn" onclick="showOnlyOneProduct(${i})">⇢</button> <br>
                 </div>`
     }`
  
@@ -49,12 +49,49 @@ function Galleri(){
     model.content = html;
     HomeView();
 }
+
+
+
+function enlargeImg(element){
+    // element.style.transform ="scale(3.0)";
+    // element.style.transition = "transform 0.25s ease";
+    element.classList.add("biggPicture")
+    element.onclick = function() {resetImg(element)}
+}
+
+function resetImg(element){
+    element.classList.remove("biggPicture")
+
+    element.onclick = function() {enlargeImg(element)}
+}
+
+function showOnlyOneProduct(index){
+
+    let html = `
+    <div class="singleProduct ">
+  
+    <button class="bckBtn btn" onclick="Galleri()">⇠</button>
+
+                <p> ${model.galleri[index].name}</p>
+                ${showImgs(index)}
+             
+                <p>Beskrivelse: ${model.galleri[index].description}</p>
+    </div>
+    `
+
+
+    model.content = html;
+    HomeView();
+}
+
 function showImgs(index){
     let html = ``
     for(let i = 0; i < model.products[index].img.length; i++){
-        html += `<div class="images">
-        <img class="pictures" src="${model.products[index].img[i]}"/>
-       </div> `
+   
+        html += `
+        <img onclick="enlargeImg(this)" class="pictures" src="${model.products[index].img[i]}"/>
+       `
     }
+    html += ``
     return html;
 }
